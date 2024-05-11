ec5e3ceb0f7c748afd1baf2ca8683717578d3b45
alex24-dev <alexssnt@gmail.com>
2024-05-11 18:19:18 -0300

    adicionando plugin do jacoco no pom.xml e criando métodos na classe TratadorDeExcecoes

--- src/main/java/br/com/empresa/api/infra/exceptions/TratadorDeExcecoes.java
+++ src/main/java/br/com/empresa/api/infra/exceptions/TratadorDeExcecoes.java
@@ -1,4 +1,39 @@
 package br.com.empresa.api.infra.exceptions;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class TratadorDeExcecoes {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity tratarJWT(Exception ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Erro: " +ex.getLocalizedMessage());
+    }
 }
