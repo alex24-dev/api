e0dd6cec97cbdb1edd835833625dd82722bd223e
alex santos <alex.ssnt@gmail.com>
2024-05-28 12:19:06 -0300

    removendo linha em branco

--- src/main/java/br/com/empresa/api/service/AutenticacaoService.java
+++ src/main/java/br/com/empresa/api/service/AutenticacaoService.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class AutenticacaoService implements UserDetailsService {
 
